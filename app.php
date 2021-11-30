04cc98ff390aa14de9f9f3bdb669bae9079d7c09
joaco753 <85913065+joaco753@users.noreply.github.com>
2021-11-30 17:34:46 -0300

    Update app.php

--- app.php
+++ app.php
@@ -1,6 +1,5 @@
 <?php
   include("./components/mainLayoutBegin.php");
-  //A partir del metodo obtener paginas del controladore iremos cambiando la pagina
   $controlador = new ControladorPaginas();
   $controlador->showView($controlador->getAccion());
   include("./components/mainLayoutEnd.php");
